a524a675ee2edd524e22b6e63496d5744725656d
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-04-06 18:19:38 +0530

    mongo url changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL = 'http://backend.zpsanglijataayu.in:5000/api';
-const MONGODB_URI = 'mongodb://backend.zpsanglijataayu.in:27017/nasha-mukti';
+const MONGODB_URI = 'mongodb://localhost:27017/nasha-mukti';
 const CLIENT_URL = 'http://backend.zpsanglijataayu.in:5173';
 
 module.exports = {

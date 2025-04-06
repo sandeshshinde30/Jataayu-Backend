c64a6f3623e82355ca35c2401509b93b50dc0f52
Sandesh Shinde <sandeshshinde7047@gmail.com>
2025-04-06 19:04:57 +0530

    cors

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://backend.zpsanglijataayu.in:5000/api';
 const MONGODB_URI = 'mongodb://localhost:27017/nasha-mukti';
-const CLIENT_URL = 'http://backend.zpsanglijataayu.in:5173';
+const CLIENT_URL = 'https://www.zpsanglijataayu.in';
 
 module.exports = {
     API_URL,
